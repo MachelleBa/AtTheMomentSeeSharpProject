e108ac820a9118aa9ed105f275717d2dc4d12ab8
Machelle <613954@student.inholland.nl>
2019-12-13 16:55:25 +0100

    Added BankRekening class (nog niet af)
    Deels skelet van BankRekening class

---
+++ Model/BetaalRekening.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtTheMomentSeeSharpSquad.Model
+{
+    class BetaalRekening
+    {
+        private string RekeningNummer;
+        // private List<Pinpas> pinpassenLijst; // = new List<Pinpas>();
+        // private List<Transactie> transactieLijst; // = new List<Transactie>();
+        // private Saldo saldo;
+        // private Gebruiker gebruiker;
+
+
+        public string getRekeningNummer() {
+
+            return this.RekeningNummer;
+        }
+
+        //gebruik cntrl+kc om een block code te commenten. Gebruik cntr+ku om te uncommenten!
+
+        //public List<Pinpas> getPinpassenLijst()
+        //{
+
+        //    return this.pinpassenLijst;
+        //}
+
+        //public List<Transactie> getTransactieLijst()
+        //{
+
+        //    return this.TransactieLijst;
+        //}
+    }
+}
