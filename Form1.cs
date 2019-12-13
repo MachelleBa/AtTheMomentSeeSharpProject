ebd92197f1a2675cfa7bc73ad77450ca1a4f7a6b
Machelle <613954@student.inholland.nl>
2019-12-13 14:28:41 +0100

    .NET Framework set-up
    met voorbeeld van gegenereerde on-click functie van een label

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtTheMomentSeeSharpSquad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //hier nooit iets voor zetten! dit moet altijd als eerste!
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "I've changed, it's me not you"; 
+        }
+    }
+}
